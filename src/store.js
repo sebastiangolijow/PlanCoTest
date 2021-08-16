0a8d87b4801b3aa3dbad6ee41182714da85170af
sebastiangolijow <you@example.com>
2021-08-16 16:24:32 +0200

    make it better

--- src/store.js
+++ src/store.js
@@ -5,7 +5,6 @@ import thunk from "redux-thunk";
 export const store = createStore(
   Reducer,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
   )
 );
